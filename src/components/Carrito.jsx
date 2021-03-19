2ce0f55e7ac1c75b063fdcc268bd21c764d31466
tony-coreno <ja.coreno@edu.utc.mx>
2021-03-19 07:00:11 -0600

    Input venta, fecha y empleado

--- src/components/Carrito.jsx
+++ src/components/Carrito.jsx
@@ -1,39 +1,41 @@
-import React from 'react';
-// import styled from 'styled-components';
+import React from "react";
+import styled from "styled-components";
 
-const Carrito = ({ventaProducto}) => {
-    return (
-        <> 
-            <h2>Carrito</h2>
+const Carrito = ({ ventaProducto }) => {
 
-            <ol>
-                    { ventaProducto.map( (venta) =>{
-                            return(
 
-                                <ul>{venta}</ul>
-                            )
+  return (
+    <>
+      <TituloEmpleado>''</TituloEmpleado>
+      <hr />
+      <h2>Carrito </h2>
+      <Contenedor>
+        <ol>
+          {ventaProducto.map((venta) => {
+            return <ul>{venta}</ul>;
+          })}
+        </ol>
+      </Contenedor>
+      <TotalDiv>
+        <input></input>
 
-                        })
-                    }
-                    </ol>
+        <h3>Total</h3>
+      </TotalDiv>
+    </>
+  );
+};
 
-        </>
-     )      
-}
+const Contenedor = styled.div`
+  padding: 15px;
+  width: 90%;
+  display: grid;
+  gap: 20px;
+  background: #eef3f5;
+  margin: 10px 0;
+  border-radius: 10px;
+  box-shadow: 0px 0px 5px rgba(129, 129, 129, 0.1);
+`;
 
-// const Contenedor = styled.div`
-//     max-width: 1200px;
-//     padding: 40px;
-//     width: 90%;
-//     display: grid;
-//     gap: 20px;
-//     grid-template-columns: 2fr 1fr;
-//     background: #fff;
-//     margin: 40px 0;
-//     border-radius: 10px;
-//     box-shadow: 0px 0px 5px rgba(129, 129, 129, 0.1);
-// `;
- 
 // const Menu = styled.nav`
 //     width: 100%;
 //     text-align: center;
@@ -45,11 +47,18 @@ const Carrito = ({ventaProducto}) => {
 //         display: inline-block;
 //         padding: 15px 20px;
 //     }
- 
+
 //     a:hover {
 //         background: #1d85e8;
 //         text-decoration: none;
 //     }
 // `;
- 
+
+const TituloEmpleado = styled.h6`
+  color: #fff;
+`;
+const TotalDiv = styled.div`
+  float: right;
+`;
+
 export default Carrito;
--- src/components/CrearVenta.jsx
+++ src/components/CrearVenta.jsx
@@ -1,31 +1,38 @@
-import React, { useState } from 'react';
-import styled from 'styled-components';
-import {Switch, NavLink, Route} from 'react-router-dom';
-import AdministrarVentas from './AdministrarVentas';
-import Reportes from './Reportes';
-import Carrito from './Carrito';
-import FormularioCrearVenta from './FormularioCrearVenta';
+import React, { useState } from "react";
+import styled from "styled-components";
+import { Switch, NavLink, Route } from "react-router-dom";
+import AdministrarVentas from "./AdministrarVentas";
+import Reportes from "./Reportes";
+import Carrito from "./Carrito";
+import FormularioCrearVenta from "./FormularioCrearVenta";
 const CrearVenta = () => {
+  const [ventaProducto, setVentaProducto] = useState([]);
+  let f = new Date();
+  return (
+    <div>
+      <Contenedorapp>
+        <Menu>
+          <NavLink to="/crear-venta">Crear Venta</NavLink>
+          <NavLink to="/administrar-venta">Administrar</NavLink>
+          <NavLink to="/reportes">Reportes</NavLink>
+        </Menu>
+        <main>
+          <Switch>
+            <Route path="/reportes" component={Reportes} />
+            <Route path="/administrar-venta" component={AdministrarVentas} />
+          </Switch>
+          <TituloEmpleado>
+            Vendedor:{" Antonio |"}  Fecha: {f.getDate() + "/" + (f.getMonth() + 1) + "/" + f.getFullYear()}
+          </TituloEmpleado>
 
-    const [ventaProducto, setVentaProducto] = useState([]);
+          <hr />
+          <h4>Escanea un producto</h4>
+          <FormularioCrearVenta
+            ventaProducto={ventaProducto}
+            setVentaProducto={setVentaProducto}
+          />
 
-    return ( 
-        <div>
-            <Contenedorapp>
-			<Menu>
-            <NavLink to="/crear-venta">Crear Venta</NavLink>
-				<NavLink to="/administrar-venta">Administrar</NavLink>
-                <NavLink to="/reportes">Reportes</NavLink>
-			</Menu>
-			<main>
-				<Switch>
-					<Route path="/reportes" component={Reportes}/>
-                    <Route path="/administrar-venta" component={AdministrarVentas}/>
-				</Switch>
-                <h4>Escanea un producto</h4>
-                <FormularioCrearVenta  ventaProducto={ventaProducto} setVentaProducto={setVentaProducto} />
-                
-                    {/* <ol>
+          {/* <ol>
                     { ventaProducto.map( (venta) =>{
                             return(
 
@@ -35,51 +42,54 @@ const CrearVenta = () => {
                         })
                     }
                     </ol> */}
-                
-			</main>
- 
-            <aside>
-                <Carrito ventaProducto={ventaProducto}/>
-            </aside>
-		</Contenedorapp>
-        </div>
-     );
-}
+        </main>
+
+        <aside>
+          <Carrito ventaProducto={ventaProducto} />
+        </aside>
+      </Contenedorapp>
+    </div>
+  );
+};
 
 const Menu = styled.nav`
-    width: 100%;
-    text-align: center;
-    background: #147551;
-    grid-column: span 2;
-    border-radius: 3px;
- 
-    a {
-        color: #fff;
-        display: inline-block;
-        padding: 15px 20px;
-    }
- 
-    a:hover {
-        background: #147571;
-        text-decoration: none;
-    }
-    a.active{
-        border-bottom: 2px solid #F2F2F2;
-        padding-bottom: 3px;
-    }
+  width: 100%;
+  text-align: center;
+  background: #147551;
+  grid-column: span 2;
+  border-radius: 3px;
+
+  a {
+    color: #fff;
+    display: inline-block;
+    padding: 15px 20px;
+  }
+
+  a:hover {
+    background: #147571;
+    text-decoration: none;
+  }
+  a.active {
+    border-bottom: 2px solid #f2f2f2;
+    padding-bottom: 3px;
+  }
 `;
- 
+
 const Contenedorapp = styled.div`
-    max-width: 1400px;
-    padding: 5px;
-    width: 100%;
-    display: grid;
-    gap: 20px;
-    grid-template-columns: 2fr 1fr;
-    background: #fff;
-    margin: 5px 0;
-    border-radius: 20px;
-    box-shadow: 0px 0px 5px rgba(129, 129, 129, 0.1);
+  max-width: 1400px;
+  padding: 5px;
+  width: 100%;
+  display: grid;
+  gap: 20px;
+  grid-template-columns: 2fr 1fr;
+  background: #fff;
+  margin: 5px 0;
+  border-radius: 20px;
+  box-shadow: 0px 0px 5px rgba(129, 129, 129, 0.1);
+`;
+
+const TituloEmpleado = styled.h6`
+  font-family: Open-sans;
 `;
 
 export default CrearVenta;
