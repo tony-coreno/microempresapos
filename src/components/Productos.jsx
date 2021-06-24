f86fb2df33d2b4c14b570b81fd04624166b0b2c4
tony-coreno <ja.coreno@edu.utc.mx>
2021-06-23 21:57:32 -0500

    creacion custom hooks separacion logica productos

--- src/components/Productos.jsx
+++ src/components/Productos.jsx
@@ -1,17 +1,16 @@
-import React, { useEffect, useState } from "react";
-// import ReactPDF from '@react-pdf/renderer';
+import React, { useEffect } from "react";
 import { Contenedorapp, Buscar } from "../products/styled/CategoriaStyle";
-import Axios from "axios";
+import { ProductosHook } from "../hooks/ProductosHook";
 import { Navbar } from "reactstrap";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faSearch } from "@fortawesome/free-solid-svg-icons";
 import HerramientasProductos from "../products/HerramientasProductos";
 import BarraProductos from "../products/BarraProductos";
 import ProductosMap from "./ProductosMap";
-// import { Page, Text, View, Document, StyleSheet } from "@react-pdf/renderer";
 
 const Productos = () => {
-  const [productos, setProductos] = useState([]);
+
+  const {productos, obtenerProductos, obtenerProductosEmpleados, buscar} = ProductosHook()
   let perfil = sessionStorage.getItem("perfil");
 
   useEffect(() => {
@@ -22,48 +21,6 @@ const Productos = () => {
     }
     // eslint-disable-next-line
   }, []);
-  const obtenerProductos = async () => {
-    const id = sessionStorage.getItem("idusuario");
-    const token = sessionStorage.getItem("token");
-    const respuesta = await Axios.get("/productos/listarporadmin/" + id, {
-      headers: { autorizacion: token },
-    });
-    setProductos(respuesta.data);
-  };
-  const obtenerProductosEmpleados = async () => {
-    const token = sessionStorage.getItem("token");
-    const jefe = sessionStorage.getItem("jefe");
-    const respuesta = await Axios.get("/productos/listarporadmin/" + jefe, {
-      headers: { autorizacion: token },
-    });
-    setProductos(respuesta.data);
-  };
-  const buscar = async (e) => {
-    if (e.target.value === "") {
-      return obtenerProductos();
-    }
-    const buscar = e.target.value;
-    const token = sessionStorage.getItem("token");
-    const respuesta = await Axios.get(
-      `/productos/buscar/${buscar}/${sessionStorage.getItem("idusuario")}`,
-      {
-        headers: { autorizacion: token },
-      }
-    );
-    setProductos(respuesta.data);
-  };
-
-  // const styles = StyleSheet.create({
-  //   page: {
-  //     flexDirection: "row",
-  //     backgroundColor: "#E4E4E4",
-  //   },
-  //   section: {
-  //     margin: 10,
-  //     padding: 10,
-  //     flexGrow: 1,
-  //   },
-  // });
 
   return (
     <div>
---
+++ src/hooks/ProductosHook.js
@@ -0,0 +1,45 @@
+import { useState } from "react";
+import Axios from "axios";
+
+export const ProductosHook = () => {
+    const [productos, setProductos] = useState([]);
+
+    const obtenerProductos = async () => {
+        const id = sessionStorage.getItem("idusuario");
+        const token = sessionStorage.getItem("token");
+        const respuesta = await Axios.get("/productos/listarporadmin/" + id, {
+          headers: { autorizacion: token },
+        });
+        setProductos(respuesta.data);
+      };
+
+      const obtenerProductosEmpleados = async () => {
+        const token = sessionStorage.getItem("token");
+        const jefe = sessionStorage.getItem("jefe");
+        const respuesta = await Axios.get("/productos/listarporadmin/" + jefe, {
+          headers: { autorizacion: token },
+        });
+        setProductos(respuesta.data);
+      };
+      const buscar = async (e) => {
+        if (e.target.value === "") {
+          return obtenerProductos();
+        }
+        const buscar = e.target.value;
+        const token = sessionStorage.getItem("token");
+        const respuesta = await Axios.get(
+          `/productos/buscar/${buscar}/${sessionStorage.getItem("idusuario")}`,
+          {
+            headers: { autorizacion: token },
+          }
+        );
+        setProductos(respuesta.data);
+      };
+    return{
+        productos,
+        obtenerProductos,
+        obtenerProductosEmpleados,
+        buscar
+    }
+
+}
